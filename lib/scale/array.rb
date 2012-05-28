1f909735db9403a5de6deca4ed3842d4eeeceb86
Michael Garriss <mgarriss@gmail.com>
2012-05-28 14:02:59 -0700

    recursive chords

--- lib/scale/array.rb
+++ lib/scale/array.rb
@@ -11,57 +11,82 @@ class Array
     end
   end
   
-  def chord(arg=nil)
-    if arg.class == Symbol
-      arg = case arg
-            when :seventh then [1,3,5,7] 
-            when :ninth then [1,3,5,7,9] 
-            when :eleventh then [1,3,5,7,9,11] 
-            when :thirteenth then [1,3,5,7,9,11,13] 
-            end
-    elsif arg.kind_of? Fixnum
-      count = [arg,size*2].min
-      arg = []
-      count.times do |i|
-        arg << ((i * 2) + 1)
+  def chord(*args)
+    arg = args.first
+    if args[1]
+      to_return = args[1..-1].map do |a|
+        a.map do |i|
+          if i.respond_to?(:chord)
+            chord(arg,i)
+          else
+            root(i).chord(arg)
+          end
+        end
       end
-    end
-    arg ||= [1,3,5]
-    arg.map do |i|
-      self[i-1]
+      to_return.size == 1 ? to_return.first : to_return
+    else
+      if arg.class == Symbol
+        arg = case arg
+              when :chord then [1,3,5] 
+              when :seventh then [1,3,5,7] 
+              when :ninth then [1,3,5,7,9] 
+              when :eleventh then [1,3,5,7,9,11] 
+              when :thirteenth then [1,3,5,7,9,11,13] 
+              end
+      elsif arg.kind_of? Fixnum
+        count = [arg,size*2].min
+        arg = []
+        count.times do |i|
+          arg << ((i * 2) + 1)
+        end
+      end
+      arg ||= [1,3,5]
+      arg.map do |i|
+        self[i-1]
+      end
     end
   end
   
   def with(arg)
-    if arg.class == Symbol
-      arg = case arg
-            when :m7 then [0,0,0,-1] 
-            when :a5 then [0,0,1] 
-            end
-    end
-    if arg.size > size
-      arg = arg[0..size]
+    if first.respond_to?(:with)
+      map do |e|
+        e.with(arg)
+      end
     else
-      arg += Array.new(size - arg.size){0}
+      if arg.class == Symbol
+        arg = case arg
+              when :m7 then [0,0,0,-1] 
+              when :a5 then [0,0,1] 
+              end
+      end
+      if arg.size > size
+        arg = arg[0..size]
+      else
+        arg += Array.new(size - arg.size){0}
+      end
+      to_return = []
+      each_with_index.map do |n,i|
+        to_return << (n.semitones + arg[i]).note
+      end
+      to_return
     end
-    to_return = []
-    each_with_index.map do |n,i|
-      to_return << (n.semitones + arg[i]).note
-    end
-    to_return
   end
   
   def %(rhs)
     with(rhs)
   end
   
-  def root(n)
-    if n.class == Symbol
+  def root(*args)
+    n = args.shift
+    to_return = if n.class == Symbol
       rotate(index(n))
     else
-      map do |e|
-        (e.semitones + n).note
-      end
+      rotate(n-1)
+    end
+    unless args.empty?
+      to_return.c(*args)
+    else
+      to_return
     end
   end
   
@@ -76,4 +101,40 @@ class Array
       e.flat
     end
   end
+  
+  def n(*args)
+    i = args.first
+    if args.size == 1 
+      e = (self[(i-1) % size])
+      to_return = if e.respond_to?(:n)
+                    to_return = map do |x|
+                      x.n(i)
+                    end
+                    if to_return.kind_of? Symbol
+                      to_return
+                    elsif to_return.size == 1
+                      to_return.first
+                    else
+                      to_return
+                    end
+                  else
+                    e
+                  end
+    else
+      if first.kind_of? Symbol
+        c(args)
+      else
+        map do |a|
+          a.n(*args)
+        end
+      end
+    end
+  end
+
+  def pretty_print(q)
+    q.text to_s
+  end
+  
+  alias c chord
+  alias r root
 end
--- spec/array_spec.rb
+++ spec/array_spec.rb
@@ -25,6 +25,7 @@ describe Array do
   describe '#chord' do
     it 'returns a new Array with the 1st, 3rd and 5th of self' do
       C.chord.should eq(C.chord([1,3,5]))
+      C.chord(:chord).should eq(C.chord([1,3,5]))
     end
     
     it 'adds the 7th when passed the arg :seventh' do
@@ -82,8 +83,9 @@ describe Array do
     C.root(:G).should eq([:G,:A,:B,:C,:D,:E,:F])
   end
   
-  it '#root returns a scale rotated to the given semitones' do
-    C.root(1).should eq([:Db,:Eb,:F,:Gb,:Ab,:Bb,:C])
+  it '#root returns a scale rotated to the given index starting at 1' do
+    C.root(1).should eq(C)
+    Cm.root(3).should eq([:Eb, :F, :G, :Ab, :Bb, :C, :D])
   end
   
   it '#sharp calls #sharp recursively' do
@@ -93,5 +95,83 @@ describe Array do
   it '#flat calls #sharp recursively' do
     [:G,:As,[:Cs,:C]].flat.should eq([:G,:Bb,[:Db,:C]])
   end
+  
+  describe '#chord with multiple array args' do
+    it 'takes an array of indexes for each arg' do
+      C.chord(:chord,[3,4]).should eq(
+        [C.root(3).chord,C.root(4).chord])
+      C.chord([1,2],[1,7]).should eq(
+        [[:C,:D],C.root(7).chord([1,2])])
+    end
+    
+    it 'works on a minor scale' do
+      Cm.chord(:chord,[1,4]).should eq(
+        [[:C,:Eb,:G],[:F,:Ab,:C]])
+    end
+    
+    it 'takes nested progression arrays' do
+      Cm.chord(:chord,[[1,4],[1,7]]).should eq(
+        [Cm.chord(:chord,[1,4]),Cm.chord(:chord,[1,7])])
+    end
+    
+    it 'treats the 3+ args as new progressions' do
+      Fs.c(2,[1,4],[5,5]).should eq(
+        [[Fs.c(2),Fs.r(4).c(2)],
+         [Fs.r(5).c(2),Fs.r(5).c(2)]])
+    end
+    
+    it 'has a chord alias == c' do
+      Cm.c(3,[[1,4],[1,7]]).should eq(
+        [Cm.chord(:chord,[1,4]),Cm.chord(:chord,[1,7])])
+    end
+    
+    it 'has a root alias == r' do
+      Fs.root(8).should eq(Fs.r(8))
+      Fs.root(1).should eq(Fs.r(1))
+    end
+  end
+  
+  it 'has a n method that return it\s nth member' do
+    C.n(1).should eq(C[0])
+    C.n(8).should eq(C[0])
+  end
+  
+  it 'r passes additional arguments to c' do
+    C.r(3,1).should eq([C[2]])
+    C.r(4,:chord,[1,4,5,4]).should eq(
+      C.r(4).c(:chord,[1,4,5,4]))        
+  end
+  
+  it 'the n method wraps' do
+    Eb.n(8).should eq(:Eb)
+  end
+  
+  it 'the r method wraps' do
+    Eb.r(8).should eq(Eb)
+  end
+  
+  it 'the n method is recursive' do
+    Fs.c(2,[1,5]).n(1).should eq([:Fs,:Cs])
+  end
+  
+  it 'the n methods takes many args and just maps' do
+    G.c(4,[1,2]).n(1,2).should eq([[:G,:B],[:A,:B]])
+  end
+  
+  it 'long example' do
+    # create the I,IV,V,VII progression of chords from the G minor
+    #   scale and then create the VI,IV,V,V progression. join these
+    #   results
+    phrase_a, phrase_b = Gm.c(3,[1,4,5,7],[6,4,5,5])
+    phrase_a.size.should eq(4)
+    phrase_b.size.should eq(4)
+    phrase_a.first.size.should eq(3)
+    phrase_a.last.size.should eq(3)
+    phrase_b.first.size.should eq(3)
+    phrase_b.last.size.should eq(3)
+    
+    section = [phrase_a] * 3
+    section << phrase_b
+    section.size.should eq(4)
+  end
 end
-
